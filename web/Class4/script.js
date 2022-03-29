1de4f226f44910c49e713e4b64dd3861213a675b
unknown <mdtahmidh7@gmail.com>
2022-03-29 11:38:32 +0600

    Flag-Flower Practice added in WebTechnology

---
+++ web/Class4/script.js
@@ -0,0 +1,9 @@
+function flag(){
+    console.log("Flag button is clicked");
+    document.getElementById("flag").style.display = "block";
+    document.getElementById("flower").style.display = "none";
+}
+function flower(){
+    document.getElementById("flower").style.display = "block";
+    document.getElementById("flag").style.display = "none";
+}
